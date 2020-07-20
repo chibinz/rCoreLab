cf5f6b0c8ae49e3a5f65995117a5b4b4d5cc51a4
chibinz <chibinzhang@hotmail.com>
2020-07-20 16:36:11 +0800

    Prototype lab 5

--- os/src/main.rs
+++ os/src/main.rs
@@ -13,39 +13,43 @@ mod memory;
 mod panic;
 mod sbi;
 mod process;
+mod drivers;
+mod fs;
 
 /// Needed if you want to define your own allocator
 extern crate alloc;
 
 use process::*;
+use memory::PhysicalAddress;
 
 global_asm!(include_str!("entry.S"));
 
 #[no_mangle]
-pub extern "C" fn rust_main() -> ! {
+pub extern "C" fn rust_main(_hart_id: usize, dtb_pa: PhysicalAddress) -> ! {
     interrupt::init();
     memory::init();
+    drivers::init(dtb_pa);
+    fs::init();
 
     let process = Process::new_kernel().unwrap();
 
-    for message in 0..8 {
-        let thread = Thread::new(
-            process.clone(),
-            sample_process as usize,
-            Some(&[message]),
-        ).unwrap();
-        PROCESSOR.get().add_thread(thread);
-    }
+    PROCESSOR
+        .get()
+        .add_thread(Thread::new(process.clone(), simple as usize, Some(&[0])).unwrap());
 
+    // 把多余的 process 引用丢弃掉
     drop(process);
 
     PROCESSOR.get().run()
 }
 
-fn sample_process(message: usize) {
-    for i in 0..1000000 {
-        if i % 200000 == 0 {
-            println!("thread {}", message);
-        }
-    }
+/// 测试任何内核线程都可以操作文件系统和驱动
+fn simple(id: usize) {
+    println!("hello from thread id {}", id);
+    // 新建一个目录
+    // 输出根文件目录内容
+    fs::ls("hello_world");
+    fs::ls("../../notebook");
+
+    loop {}
 }
